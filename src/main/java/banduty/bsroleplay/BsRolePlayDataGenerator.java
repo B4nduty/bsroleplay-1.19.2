d72050501b24d8a4e8dae0803666f2eb888b1013
B4nduty <bandutysmods@gmail.com>
2024-04-21 19:40:32 +0200

    Version 1.5.5 1.19.2a0.0.9

--- src/main/java/banduty/bsroleplay/BsRolePlayDataGenerator.java
+++ src/main/java/banduty/bsroleplay/BsRolePlayDataGenerator.java
@@ -1,8 +1,9 @@
 package banduty.bsroleplay;
 
 import banduty.bsroleplay.datagen.AdvancementsProvider;
+import banduty.bsroleplay.datagen.ModBlockTagProvider;
+import banduty.bsroleplay.datagen.ModLootTableProvider;
 import banduty.bsroleplay.datagen.ModModelProvider;
-import banduty.bsroleplay.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -10,8 +11,9 @@ public class BsRolePlayDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
-		fabricDataGenerator.addProvider(ModRecipeProvider::new);
 		fabricDataGenerator.addProvider(ModModelProvider::new);
 		fabricDataGenerator.addProvider(AdvancementsProvider::new);
+		fabricDataGenerator.addProvider(ModLootTableProvider::new);
+		fabricDataGenerator.addProvider(ModBlockTagProvider::new);
 	}
 }
